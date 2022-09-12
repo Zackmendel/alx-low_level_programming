25e537dc7ff50f27438e0319600c9d391b908af1
Isaac <samuelisaac1995@gmail.com>
2022-09-12 14:23:20 -0700

    My alx variables if, else, while 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,10 @@
 int main(void)
 {
 	char ch;
-	/* sets loop for alphabets*/
+
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-	putchar(ch);
-	putchar('\n');
-	}
+		putchar(ch);
+		putchar('\n');
+		
 	return (0);
 }
