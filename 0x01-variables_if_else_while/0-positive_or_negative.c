e3955095d0ca06ef826ca0370c2b0c77181c22d0
Isaac <samuelisaac1995@gmail.com>
2022-09-12 10:36:18 -0700

    My alx variables if, else, while 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/* main - Entry point */
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -10,16 +14,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if(n > 0) {
+	if (n > 0) 
+	{
 	/*if condition is true, print:*/
 		printf("%d is positive\n", n);
 	}
-	else if(n == 0) {
+	else if (n == 0) 
+	{
 	/*if condition is true, print:*/
 		printf("%d is zero\n", n);
 	}
-	else {
-		printf("%d is negative", n);
+	else 
+	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
