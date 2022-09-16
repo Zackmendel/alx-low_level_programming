dcaabbadeefc7e3f0dd2e9f65f4fdcc89e04f8f4
Isaac <samuelisaac1995@gmail.com>
2022-09-16 14:33:36 -0700

    my alx functions nested loops 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - check the code
  *
  * Return: Always0 (Success)
  */
-void print_alphabet(void)
+int main(void)
 {
 	char i;
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 		_putchar('\n');
+	return (0);
 }
