142ed0a6b6a1a336e70cd194415edebfc8d62218
Isaac <samuelisaac1995@gmail.com>
2022-09-16 15:10:51 -0700

    my alx functions nested loops 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main - check the code
+ * Description: uses _putchar function to print alphabet
+ * in lowercase 10 times
+ * Return: always o.
+ */
+int main(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j< 10; j++)
+	{
+		for (i ='a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+	print_alphabet_x10();
+	return (0);
+}
