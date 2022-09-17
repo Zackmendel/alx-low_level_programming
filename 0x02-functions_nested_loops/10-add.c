669cf9ee3aa9f3b477ea3f5bbfb6e05286babbb4
Isaac <samuelisaac1995@gmail.com>
2022-09-16 22:18:09 -0700

    my alx functions nested loops 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - short description
+ * @a: an integer
+ * @b: an integer
+ * function that adds two integers, a & b and returns the result
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
