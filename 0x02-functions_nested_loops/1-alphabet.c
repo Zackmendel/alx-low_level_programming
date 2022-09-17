ea6fa1b1cf351967b6fa0610853fc699376d5696
Isaac <samuelisaac1995@gmail.com>
2022-09-16 19:42:49 -0700

    my alx functions nested loops 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code
- * Description: Prints alphabet in lowercase followed by a new line
+ * print_alphabet prints alphabet in lowercase followed by a new line
+ *
  * Return: Always0 (Success)
  */
 void print_alphabet(void)
@@ -10,8 +10,6 @@ void print_alphabet(void)
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-	{
 		_putchar(i);
 	_putchar('\n');
-	}
 }
