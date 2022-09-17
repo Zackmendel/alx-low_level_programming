34c81677e9b56ffbef2fcd3bbcab0aa02cb3cf66
Isaac <samuelisaac1995@gmail.com>
2022-09-16 20:55:48 -0700

    my alx functions nested loops 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int _abs(int) - short description
+ * function computes the absolute value of an integer
+ * Return: Absolute value of number
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (n * -1);
+}
