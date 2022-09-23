996f37053656c21b9e5a49a43f15331fe6ac0f03
Isaac <samuelisaac1995@gmail.com>
2022-09-22 22:53:08 -0700

    My pointers, arrays, strings 7

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-	char *ptr;
+	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char *p;
 
 	ptr = cap_string(s);
 	printf("%s", p);
