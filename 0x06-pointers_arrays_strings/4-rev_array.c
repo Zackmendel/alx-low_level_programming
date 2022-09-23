695cd448ed75c08292d2ffd1f3e196fa10ecbe8b
Isaac <samuelisaac1995@gmail.com>
2022-09-22 21:46:03 -0700

    My pointers, arrays, strings 6

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse array - a function that reverse an array
+ *
+ * @a: input array
+ * @n: number of elements of array
+ *
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int aux;
+
+	while (i < n--)
+	{
+		aux = a[i];
+		a[i++] = a[n];
+		a[n] = aux;
+	}
+}
